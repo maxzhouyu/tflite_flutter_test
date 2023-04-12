655b03d6f34fc7376e9593cbe50f5cee97918163
Max Zhou Yu <max@vivino.cn.com>
2023-04-12 09:13:47 +0800

    pub

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:tflite_flutter/tflite_flutter.dart';
 
-void main()=>runApp(const MaterialApp(
-  home: HomePage(),
-));
+void main() => runApp(const MaterialApp(
+      home: HomePage(),
+    ));
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -13,9 +14,23 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   @override
+  void initState() async {
+    final gpuDelegateV2 = GpuDelegateV2(options: GpuDelegateOptionsV2());
+
+    var interpreterOptions = InterpreterOptions()..addDelegate(gpuDelegateV2);
+    final interpreter = await Interpreter.fromAsset('your_model.tflite',
+        options: interpreterOptions);
+
+
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('tflite_flutter'),),
+      appBar: AppBar(
+        title: const Text('tflite_flutter'),
+      ),
       body: Container(),
     );
   }
